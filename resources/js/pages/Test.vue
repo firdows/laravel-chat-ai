7bec728c500cbff8d39f3ccc8e6f3d5b82b363e8
Ake.A <ahamad.jehduaramae@ssup.co.th>
2025-08-30 18:05:17 +0700

    add page test

--- resources/js/pages/Test.vue
+++ resources/js/pages/Test.vue
@@ -1,11 +1,32 @@
-<template lang="">
-    <div>
-        Test
-    </div>
-</template>
-<script setup>
 
+<template>
+    <Head title="Test" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                Test
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { dashboard,home,test } from '@/routes';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Home',
+        href: home().url,
+    },
+    {
+        title: 'Test',
+        href: test().url,
+    },
+];
 </script>
-<style lang="">
-    
-</style>
